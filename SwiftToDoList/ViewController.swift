1765a3d2602467fc9075a13f87faa9c585569619
Abi Radzhabova <aradzhabova@askartec.com>
2020-04-21 12:04:03 +0600

    Initial Commit

---
+++ SwiftToDoList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftToDoList
+//
+//  Created by Abi  Radzhabova on 4/21/20.
+//  Copyright © 2020 Abi  Radzhabova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
